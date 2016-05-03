72767ac8e7c97a075b7d00c1d758df9353cdbca2
jonasms <jonas.sota@gmail.com>
2016-05-02 17:39:13 -0700

    Create updatePosts(), Create subitBtn onclick function

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,33 +1,70 @@
 // YOUR CODE HERE:
+var $posts = $('.posts')[0];
+var $input = $('.chatInput')[0];
 
 var message = {
   username: 'netflix&chill',
-  text: 'whatever bro',
-  roomname: 'broski'
+  text: '',
+  roomname: 'lobby'
 };
-
-$.ajax({
-  // This is the url you should use to communicate with the parse API server.
-  url: 'https://api.parse.com/1/classes/messages',
-  type: 'POST',
-  data: JSON.stringify(message),
-  contentType: 'application/json',
-  success: function (data) {
-    console.log('chatterbox: Message sent');
-  },
-  error: function (data) {
-    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-    console.error('chatterbox: Failed to send message', data);
-  }
-});
 
 // var responses = $.get('https://api.parse.com/1/classes/messages', function(data) {
 //   return data;
 // });
+var newPosts = {};
 
-$.get('https://api.parse.com/1/classes/messages', {}, function(data) { 
-  $('.post').append(data.results[0].text);
+var updatePosts = function() {
+  //make a GET request && post result
+  $.get('https://api.parse.com/1/classes/messages', {}, function(data) {
+
+    for (let post of data.results) {
+      var encodedId = _.escape(post.objectId);
+      if (!newPosts[encodedId]) {
+      //get all messages
+        var encodedName = _.escape(data.results[0].username);
+        var encodedText = _.escape(data.results[0].text);
+        newPosts[encodedId] = true; 
+      //filter out old messages from new messages
+
+
+        $('.posts').append('<div class="post">' + encodedText + '</div>');
+      }
+    }    
+  });
+    
+};
+
+$('.submitBtn').click(function() {
+  //save value from chatInput
+  var textMessage = $input.value;
+  //add value to message object
+  message.text = textMessage;
+
+  //submit a ajax post request of message
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: 'https://api.parse.com/1/classes/messages',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+  
+  //clear $input
+  $input.value = '';
+  //TODO: call ajax GET request on click
+  updatePosts();
+
 });
+
+setInterval(function() { updatePosts(); }, 100);
+
 
 
 //for every element in responses.responseJSON.results
